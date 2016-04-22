2a2b32b8e48d52a4a2e1bff6d71923c28320a7b9
MsahiroOsanai <osanai.masahiro.kakaku@gmail.com>
2016-04-22 02:41:20 +0000

    Finished challenge2

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,6 @@
 class Message < ActiveRecord::Base
     validates :name, length: {maximum: 20}, presence: true
-    validates :body, length: {minimum: 2, maximum: 30} , presence: true
+    validates :body, length: {minimum: 2, maximum: 30} , presence: true ,uniqueness: true
+    validates :age, length: {maximum:2} , presence: true ,
+    numericality: { only_integer: true , greater_than: 0}
 end
