f3967c4b9f72131e4e54cc61bb94c18fcf79b8b4
MsahiroOsanai <osanai.masahiro.kakaku@gmail.com>
2016-04-22 04:13:10 +0000

    re submit

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,5 @@ class Message < ActiveRecord::Base
     validates :name, length: {maximum: 20}, presence: true
     validates :body, length: {minimum: 2, maximum: 30} , presence: true ,uniqueness: true
     validates :age, length: {maximum:2} , presence: true ,
-    numericality: { only_integer: true , greater_than: 0}
+    numericality: { only_integer: true , greater_than: -1}
 end
